6bb48e6c875da6a3ae87269b327e9a62117e819d
Florian BARRE <florian.barre78@gmail.com>
2023-01-09 15:20:20 +0100

    Change history size feature

--- tools/app_components/app.py
+++ tools/app_components/app.py
@@ -11,7 +11,7 @@ from tools.utilities import get_current_date
 def write_topic():
     if APP_CONFIG.TOKEN != request.args.get('token'):
         return jsonify(status="Error auth", state=None), APP_CONFIG.CODE_ERROR["unauthorize"]
-    msg = "topic's writter doesn't work, an error occured"
+    msg = "topic's writer doesn't work, an error occured"
 
     topic = request.args.get('topic')
     topic = topic.replace("$", "/")
@@ -43,7 +43,7 @@ def write_topic():
         else:
             print(f"To many {topic}, what is the matter ?")
 
-        msg = "topic's writter works successfully"
+        msg = "topic's writer works successfully"
 
     except KeyError as err:
         print(f"ERROR - write_topic: {err}")
@@ -78,6 +78,9 @@ def read_topic():
             history_topic_result = db.session.query(History).filter(
                 getattr(History, "topic") == topic).order_by(getattr(History, "timestamp").desc()).all()
 
+            # Get topic's history size in general topics
+            history_size = general_topic_result[0].history_size
+
             # Take the state, last state or state index which was asked
             if previous_state_index is None:
                 state = history_topic_result[0].state
@@ -89,20 +92,24 @@ def read_topic():
             if state is None:
                 state = "null"
 
+
         elif len(general_topic_result) == 0:
             # Add the new topic
             add_topic(db.session, topic)
             state = "null"
+            history_size = APP_CONFIG.GLOBAL["default_history_size"]
 
         else:
             print(f"To many {topic}, what is the matter ?")
 
         if parse_arg is not None:
-            return jsonify(status="topic's reader works successfully", state=state).json[parse_arg], \
-                   APP_CONFIG.CODE_ERROR["successfully_request"]
+            return \
+            jsonify(status="topic's reader works successfully", state=state, history_size=str(history_size)).json[
+                parse_arg], APP_CONFIG.CODE_ERROR["successfully_request"]
         else:
-            return jsonify(status="topic's reader works successfully", state=state), APP_CONFIG.CODE_ERROR[
-                "successfully_request"]
+            return jsonify(status="topic's reader works successfully", state=state, history_size=history_size), \
+                   APP_CONFIG.CODE_ERROR[
+                       "successfully_request"]
 
     except KeyError as err:
         print(f"ERROR - read_topic: {err}")
@@ -151,4 +158,45 @@ def delete_topic():
 
     except KeyError as err:
         print(f"ERROR - read_topic: {err}")
-        return jsonify(status="topic's reader doesn't work, an error occured"), APP_CONFIG.CODE_ERROR["crash"]
+        return jsonify(status="topic's delete doesn't work, an error occured"), APP_CONFIG.CODE_ERROR["crash"]
+
+
+@app.route('/api/history_size', methods=['GET'])
+def change_history_size():
+    if APP_CONFIG.TOKEN != request.args.get('token'):
+        return jsonify(status="Error auth", state=None), APP_CONFIG.CODE_ERROR["unauthorize"]
+
+    topic = request.args.get('topic')
+    topic = topic.replace("$", "/")
+    size = request.args.get('size')
+
+    if topic is None:
+        return jsonify(status="Error topic parameter is missing"), APP_CONFIG.CODE_ERROR["missing_parameter"]
+
+    elif size is None or size == "" or int(size) < 0:
+        return jsonify(status="Error new history size is incorrect"), APP_CONFIG.CODE_ERROR["missing_parameter"]
+
+    try:
+        # Check is the topic exist in the general topics table
+        general_topic_result = db.session.query(Topics).filter(getattr(Topics, "topic") == topic).all()
+
+        # If it exists
+        if len(general_topic_result) == 1:
+            # Change history size
+            general_topic_result[0].history_size = size
+            db.session.commit()
+
+        # If doesn t exist
+        elif len(general_topic_result) == 0:
+            # Add the new topic
+            add_topic(db.session, topic, default_history_size=size)
+
+        # If there is to many topics create an error
+        else:
+            print(f"To many {topic}, what is the matter ?")
+
+        return jsonify(status="history size changer work successfully"), APP_CONFIG.CODE_ERROR["successfully_request"]
+
+    except KeyError as err:
+        print(f"ERROR - read_topic: {err}")
+        return jsonify(status="History size changer doesn't work, an error occured"), APP_CONFIG.CODE_ERROR["crash"]
