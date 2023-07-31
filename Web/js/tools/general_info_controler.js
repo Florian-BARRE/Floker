aa9354f66aa9f84932ae1312349d8a4d20deee49
Florian-BARRE <94544310+Florian-BARRE@users.noreply.github.com>
2023-07-31 08:52:41 +0200

    Delete Web directory

--- Web/js/tools/general_info_controler.js
+++
@@ -1,5 +0,0 @@
-function general_info_update(infos, skip_first_row = true){
-    update_nb_rows(GENERAL_INFO_TABLE_ID, infos.length + skip_first_row, GENERAL_INFOS_TEMPLATE.length);
-    set_row(GENERAL_INFO_TABLE_ID, Number(skip_first_row), infos);
-
-}
