acadcda03f51f711e0f89f503b422351a1b763cf
Florian BARRE <florian.barre78@gmail.com>
2023-10-09 19:29:25 +0300

    New feature : database viewer Patch bug : error when no topic parameter New folder to store endpoints

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from cheroot.wsgi import Server as WSGIServer, PathInfoDispatcher
 
+
 from tools.app_components.history_size_supervizor import start_history_size_supervisor
 from tools.sql import app, db
 from configuration import APP_CONFIG
@@ -9,12 +10,12 @@ if __name__ == '__main__':
     Thread(target=start_history_size_supervisor, args=(app, db)).start()
 
     with app.app_context():
-
         if APP_CONFIG.SERVER_MODE == "debug_server":
             app.run(host='0.0.0.0', port=APP_CONFIG.GLOBAL["listen_port"], debug=True)
 
         elif APP_CONFIG.SERVER_MODE == "cherrypy_server":
             server = WSGIServer(('0.0.0.0', APP_CONFIG.GLOBAL["listen_port"]), PathInfoDispatcher({'/': app}))
+            server.maxthreads = -1
             try:
                 server.start()
             except KeyboardInterrupt:
--- tools/sql/table.py
+++ tools/sql/table.py
@@ -5,7 +5,7 @@ class Topics(db.Model):
     __tablename__ = 'topics'
     id = db.Column(db.Integer, primary_key=True)
     # Attributs
-    topic = db.Column(db.String, nullable=True)
+    topic = db.Column(db.String(1000), nullable=True)
     history_size = db.Column(db.Integer, nullable=False)
 
 
@@ -13,7 +13,7 @@ class History(db.Model):
     __tablename__ = 'history'
     id = db.Column(db.Integer, primary_key=True)
     # Attributs
-    topic = db.Column(db.String, nullable=False)
-    state = db.Column(db.String, nullable=True)
+    topic = db.Column(db.String(1000), nullable=False)
+    state = db.Column(db.String(10000), nullable=True)
     date = db.Column(db.Date, nullable=False)
     timestamp = db.Column(db.Integer, nullable=False)
